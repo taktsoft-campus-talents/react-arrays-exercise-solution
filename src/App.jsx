6f4d16a7fc9a9ce15c69dcb93a27d0497704cab2
Ralf Siewert <ralf@actyvy.st>
2025-02-04 11:29:37 +0100

    add filter buttons and state for active filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+import { users } from "./data/users";
 
 // Good luck with the exercise 💪
 function App() {
-  return <></>;
+  // "all", "men", "women", "by-age", "by-name"
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  // should return an array of users, filtered or sorted according to the activeFilter state
+  function applyFilters() {
+    return users;
+  }
+
+  const filteredUsers = applyFilters();
+
+  return (
+    <>
+      <header>
+        <h1>React Array Exercise</h1>
+      </header>
+      <main>
+        <section className="filter-container">
+          <button>All</button>
+          <button>Women</button>
+          <button>Men</button>
+          <button>By Name</button>
+          <button>By Age</button>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
